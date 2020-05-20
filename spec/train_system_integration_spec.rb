c2b7c4a54f5365a2bc1787aa3fc34a26d5034cf5
Daniel Merys <dkmerys@gmail.com>
2020-05-20 16:06:17 -0700

    pass all integration tests

--- spec/train_system_integration_spec.rb
+++ spec/train_system_integration_spec.rb
@@ -50,17 +50,13 @@ describe('update and delete a train path', {:type => :feature}) do
   it('changes a trains name, then deletes it') do
     train = Train.new({:name => "Thomas", :id => nil})
     train.save
-    city = City.new({:name => "Oregon City", :train_id => train.id, :id => nil})
-    city.save
     visit("/trains")
-    click_on("See all Cities")
-    click_on("Oregon City")
-    click_link("Edit City")
-    fill_in("city_name", :with => "Carson City")
+    click_on("Thomas")
+    click_link("Edit Train")
+    fill_in("train_name", :with => "Bob")
     click_on("Update")
-    click_on("Carson City")
-    click_link("Edit City")
-    click_on("Delete city")
-    expect(page).to have_no_content("Carson City")
+    click_link("Edit Train")
+    click_on("Delete train")
+    expect(page).to have_no_content("Thomas")
   end
 end
